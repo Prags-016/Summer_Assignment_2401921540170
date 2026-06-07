8ad872e2076592822db11f373c4effd1a95a2f4f
Prags-016 <pragyarajput870@gmail.com>
2026-06-07 14:26:09 +0530

    Restore prb14 and prb15 code

---
+++ WEEK_1/Day_5/prb14.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int st = 0;
+        int e = s.size() - 1;
+
+        while (st < e) {
+            swap(s[st++], s[e--]);
+        }
+    }
+};
+
+int main() {
+    vector<char> s = {'h', 'e', 'l', 'l', 'o'};
+
+    Solution obj;
+    obj.reverseString(s);
+
+    cout << "Reversed String: ";
+
+    for (char ch : s) {
+        cout << ch;
+    }
+
+    cout << endl;
+
+    return 0;
+}
---
+++ WEEK_1/Day_5/prb15.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+class ListNode {
+public:
+    int val;
+    ListNode* next;
+
+    ListNode(int x) {
+        val = x;
+        next = NULL;
+    }
+};
+
+class Solution {
+public:
+    bool hasCycle(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
+
+int main() {
+    // Creating nodes
+    ListNode* head = new ListNode(3);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(0);
+    head->next->next->next = new ListNode(-4);
+
+    // Creating cycle: -4 -> 2
+    head->next->next->next->next = head->next;
+
+    Solution obj;
+
+    if (obj.hasCycle(head))
+        cout << "Cycle Detected" << endl;
+    else
+        cout << "No Cycle" << endl;
+
+    return 0;
+}
